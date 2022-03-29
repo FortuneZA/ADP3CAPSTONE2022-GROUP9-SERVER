549b3ec1213d82de254bc0e58d98871b508db2b3
Cameron Noemdo <219115443@mycput.ac.za>
2022-03-29 14:24:15 +0200

    Updated Lecturer Factory test class

--- src/test/java/za/ac/cput/Factory/LecturerFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/LecturerFactoryTest.java
@@ -1,5 +1,12 @@
 package za.ac.cput.Factory;
 
+/* LecturerFactoryTest.java
+Test class for the Lecturer Factory
+Author: Cameron Henry Noemdo (219115443)
+Date: 29 March 2022
+*/
+
+import org.apache.commons.validator.routines.EmailValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -10,19 +17,42 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LecturerFactoryTest {
-    Lecturer lecturer1,lecturer2,lecturer3; // object creation
+    Lecturer lecturer1,lecturer2,lecturer3,test; // object creation
 
     @BeforeEach
     void setUp() {  // object instantiation
         lecturer1= LecturerFactory.createLecturer("Cameron","Noemdo", "cn@school.com");
         lecturer2= LecturerFactory.createLecturer("John","Doe", "jd@school.com");
         lecturer3=lecturer1;
+        test=LecturerFactory.createLecturer("J","D","jdschool.com"); // NOTE: Incorrect email address format
     }
 
     @Test
-    void testCreation() // test object creation
+    void testObjectCreation() // test object creation
     {
         assertNotNull(lecturer1);
+    }
+
+    @Test
+    void testCreateLecturer()
+    {
+        assertNotNull(lecturer1.getLecturerID());
+        assertEquals("Cameron",lecturer1.getFirstName());
+        assertEquals("Noemdo",lecturer1.getLastName());
+        assertEquals("cn@school.com",lecturer1.getLecturerEmail());
+    }
+
+    @Disabled("Test disabled as it will fail (Correct outcome).")
+    @Test
+    void testEmailFail()
+    {
+        assertTrue(EmailValidator.getInstance().isValid(test.getLecturerEmail()));
+    }
+
+    @Test
+    void testEmailPass()
+    {
+        assertTrue(EmailValidator.getInstance().isValid(lecturer1.getLecturerEmail()));
     }
 
     @Test
@@ -53,13 +83,5 @@ public class LecturerFactoryTest {
             System.out.println("Exception: "+iEx.getMessage());
             System.out.println("Test timed out");
         }
-    }
-
-    @Test
-    @Disabled
-    void disableTest()
-    {
-        assertNotNull(lecturer3);
-        System.out.println("Test successfully disabled");
     }
 }
