90e55c8a536b108048f14995b4b85285ca44ae3b
Cameron Noemdo <219115443@mycput.ac.za>
2022-03-29 20:19:44 +0200

    Added author details for the ILecturerRepository, IUniversityRepository, LecturerFactory, and UniversityFactory files

--- src/main/java/za/ac/cput/impl/ILecturerRepository.java
+++ src/main/java/za/ac/cput/impl/ILecturerRepository.java
@@ -1,5 +1,11 @@
 package za.ac.cput.impl;
 
+/* ILecturerRepository.java
+Implementation of Repository for the Lecturer
+Author: Cameron Henry Noemdo (219115443)
+Date: 29 March 2022
+*/
+
 import za.ac.cput.Entity.Lecturer;
 import za.ac.cput.Repository.IRepository;
 
--- src/main/java/za/ac/cput/impl/IUniversityRepository.java
+++ src/main/java/za/ac/cput/impl/IUniversityRepository.java
@@ -1,5 +1,11 @@
 package za.ac.cput.impl;
 
+/* IUniversityRepository.java
+Implementation of Repository for the University
+Author: Cameron Henry Noemdo (219115443)
+Date: 29 March 2022
+*/
+
 import za.ac.cput.Entity.University;
 import za.ac.cput.Repository.IRepository;
 
