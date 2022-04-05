8fa0d6beee7b8a0325f8f9732f613cc212f92f67
User <217238173@mycput.ac.za>
2022-04-05 18:00:32 +0200

    second commit

---
+++ src/main/java/za/ac/cput/Factory/TestFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.Factory;
+import za.ac.cput.Entity.Test;
+import za.ac.cput.Util.GenericHelper;
+/*
+Factory for Test
+Author: Themba Kanyile 217238173
+Date : 23 March 2022
+ */
+public class TestFactory
+{
+
+    public static Test createTest(String lecturerId, String testInfo){
+
+        String testId = GenericHelper.generateID();
+
+        Test test = new Test.Builder()
+                .setTestId(testId)
+                .setLecturerId(lecturerId)
+                .setTestInfo(testInfo)
+                .build();
+
+        return test;
+    }
+}
--- src/main/java/za/ac/cput/Util/GenericHelper.java
+++ src/main/java/za/ac/cput/Util/GenericHelper.java
@@ -1,4 +1,10 @@
 package za.ac.cput.Util;
+import java.util.UUID;
+public class GenericHelper
+{
+    //student
+    public static String generateID(){
+        return UUID.randomUUID().toString();
 
-public class GenericHelper {
+    }
 }
