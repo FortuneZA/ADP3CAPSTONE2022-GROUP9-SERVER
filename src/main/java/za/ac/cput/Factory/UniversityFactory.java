cb25abcef51696b75e95fe00b270d74ac5405885
Cameron Noemdo <219115443@mycput.ac.za>
2022-07-28 12:39:50 +0200

    Updated university factory class

--- src/main/java/za/ac/cput/Factory/UniversityFactory.java
+++ src/main/java/za/ac/cput/Factory/UniversityFactory.java
@@ -6,6 +6,7 @@ Author: Cameron Henry Noemdo (219115443)
 Date: 29 March 2022
 */
 
+import org.springframework.util.StringUtils;
 import za.ac.cput.Entity.University;
 import za.ac.cput.Util.GenericHelper;
 
@@ -13,22 +14,22 @@ public class UniversityFactory {
     public static University createUniversity(String universityName, String city, String address, String phoneNumber)
     {
         String universityID = GenericHelper.generateID();
-        if(universityName.isEmpty()
-                ||city.isEmpty()
-                ||address.isEmpty()
-                ||phoneNumber.isEmpty()
-                || !(phoneNumber.replaceAll("\\s+","").length() >=10 && phoneNumber.replaceAll("\\s+","").length() <=12)) // before checking length, whitespaces are replaced with no space
-            {
-            return null;
-        }
-        else {
-            return new University.Builder()
-                    .setUniversityID(universityID)
-                    .setUniversityName(universityName)
-                    .setCity(city)
-                    .setAddress(address)
-                    .setPhoneNumber(phoneNumber)
-                    .build();
-        }
+        if(!StringUtils.hasLength(universityName))
+            throw new IllegalArgumentException("University name is not present");
+        if(!StringUtils.hasLength(city))
+            throw new IllegalArgumentException("City is not present");
+        if(!StringUtils.hasLength(address))
+            throw new IllegalArgumentException("Address is not present");
+        if(!StringUtils.hasLength(phoneNumber))
+            throw new IllegalArgumentException("Phone number is not present");
+        if(!(phoneNumber.replaceAll("\\s+","").length() == 10 || phoneNumber.replaceAll("\\s+","").length() == 12))
+            throw new IllegalArgumentException("Invalid phone number length provided");
+        return new University.Builder()
+                .setUniversityID(universityID)
+                .setUniversityName(universityName)
+                .setCity(city)
+                .setAddress(address)
+                .setPhoneNumber(phoneNumber)
+                .build();
     }
 }
