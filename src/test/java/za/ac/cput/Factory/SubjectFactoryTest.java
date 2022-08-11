59ac6a49f15ecc6b053ecfdaf4eb951cf695c574
Project3Assignment <mfortuin509@gmail.com>
2022-08-11 20:59:20 +0200

    Updated Course and Factory Test

--- src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
@@ -4,37 +4,41 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.Entity.Subject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SubjectFactoryTest {
 
-    //OBJECT CREATION
-    Subject subject = SubjectFactory.createSubject("INM351","Information Management",36,"AF10");
 
-    //OBJECT EAQUALITY
+    private final  Subject subject = SubjectFactory.createSubject("INM351","Information Management",36,"AF10");
+    private final  Subject subject2 = SubjectFactory.createSubject("ITS201","Information Systems",36,"WR10");
+
+    private Subject subject1;
+
+    //Successful Create
     @Test
-    public void ObjectEquality()
+     void successfulCreateSubject()
     {
-        Subject subject2 = SubjectFactory.createSubject("INM351","Information Management",36,"AF10");
-
-        assertNotEquals(subject2,subject);
+        assertAll(
+                ()->assertNotNull(subject),
+                ()->assertEquals("INM351",subject.getSubjectID())
+        );
     }
 
-    //OBJECT IDENTITY
+    //Failure create
     @Test
-    public void ObjectIdentity()
+    void failCreateSubject()
     {
-        Subject subject2=subject;
-
-        assertEquals(subject2,subject);
+        Exception exception=assertThrows(IllegalArgumentException.class,()->subject1=SubjectFactory.createSubject("ITS201","Information Systems", 36,"WR10"));
+        System.out.println(exception.getMessage());
+        assertTrue(exception.getMessage().contains("Subject Name was not provided"));
     }
 
     //TIMEOUT
     @Test
     public void Timeout()
     {
-        ObjectIdentity();
+        successfulCreateSubject();
     }
 
     //DISABLED
@@ -42,12 +46,10 @@ public class SubjectFactoryTest {
     @Disabled
     public void Disabled()
     {
-        Subject subject2 = SubjectFactory.createSubject("INM351","Information Management",36,"AF10");
-
-        assertEquals(subject2,subject);
+       failCreateSubject();
     }
 
-    //BUILD VERIFICATION
+    //Build Verification
     @Test
     public void TestNull()
     {
