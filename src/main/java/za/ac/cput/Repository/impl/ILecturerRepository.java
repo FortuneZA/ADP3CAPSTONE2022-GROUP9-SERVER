c98a2132f33eefa22fea3eaf252a0e9081b002e9
Cameron Noemdo <219115443@mycput.ac.za>
2022-08-21 18:11:35 +0200

    Completed Lecturer repository implementation

--- src/main/java/za/ac/cput/Repository/impl/ILecturerRepository.java
+++ src/main/java/za/ac/cput/Repository/impl/ILecturerRepository.java
@@ -1,6 +1,13 @@
 package za.ac.cput.Repository.impl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.Entity.Lecturer;
+
+import java.util.Set;
+
 /*
 
  */
-public interface ILecturerRepository {
+public interface ILecturerRepository extends JpaRepository<Lecturer, String> {
+    Set<Lecturer> getAll();
 }
