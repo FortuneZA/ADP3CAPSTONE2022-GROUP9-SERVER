e00f175fea01d9e42530a06d884d27420aea9230
Cameron Noemdo <219115443@mycput.ac.za>
2022-08-21 19:35:34 +0200

    Completed UniversityService

--- src/main/java/za/ac/cput/service/UniversityService.java
+++ src/main/java/za/ac/cput/service/UniversityService.java
@@ -1,6 +1,54 @@
 package za.ac.cput.service;
-/*
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.Entity.University;
+import za.ac.cput.Repository.impl.IUniversityRepository;
+import za.ac.cput.service.impl.IUniversityService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/*
+    UniversityService.java
+    Service for the University
+    Author: Cameron Henry Noemdo (219115443)
+    Date: 21 August 2022
  */
-public class UniversityService {
+@Service
+public class UniversityService implements IUniversityService {
+
+    @Autowired
+    private IUniversityRepository universityRepository;
+
+    @Override
+    public University create(University university) {
+        return this.universityRepository.save(university);
+    }
+
+    @Override
+    public University read(String universityName) {
+        return this.universityRepository.findById(universityName).orElseGet(null);
+    }
+
+    @Override
+    public University update(University university) {
+        return create(university);
+    }
+
+    @Override
+    public boolean delete(String universityName) {
+        this.universityRepository.deleteById(universityName);
+        if(this.universityRepository.existsById(universityName)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public Set<University> getAll() {
+        return this.universityRepository.findAll().stream().collect(Collectors.toSet());
+    }
 }
