a8142eb5b5a8da791089b847d88d92fc65802280
Cameron Noemdo <219115443@mycput.ac.za>
2022-08-21 18:16:15 +0200

    Added author details

--- src/main/java/za/ac/cput/Repository/impl/ILecturerRepository.java
+++ src/main/java/za/ac/cput/Repository/impl/ILecturerRepository.java
@@ -6,7 +6,10 @@ import za.ac.cput.Entity.Lecturer;
 import java.util.Set;
 
 /*
-
+    ILecturerRepository.java
+    Repository for the Lecturer
+    Author: Cameron Henry Noemdo (219115443)
+    Date: 21 August 2022
  */
 public interface ILecturerRepository extends JpaRepository<Lecturer, String> {
     Set<Lecturer> getAll();
