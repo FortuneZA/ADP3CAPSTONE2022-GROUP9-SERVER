7aadc5068ea8fd53a23705ed6922aab5f56ee706
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-12 22:21:01 +0200

    Update TestController.java

--- src/main/java/za/ac/cput/controller/TestController.java
+++ src/main/java/za/ac/cput/controller/TestController.java
@@ -1,4 +1,10 @@
 package za.ac.cput.controller;
+/*
+ * Name:Themba
+ * Surname:Khanyile
+ * StudentNumber:217238173
+ * Date: 12 October 2022
+ */
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
