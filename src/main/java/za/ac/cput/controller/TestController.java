1bf818e23636351ff924a613c6ee04c065883af7
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-12 22:18:08 +0200

    Update TestController.java

--- src/main/java/za/ac/cput/controller/TestController.java
+++ src/main/java/za/ac/cput/controller/TestController.java
@@ -1,4 +1,48 @@
 package za.ac.cput.controller;
 
-public class TestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.entity.Course;
+import za.ac.cput.service.impl.CourseService;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/test")
+public class TestController 
+{
+  
+    @Autowired
+    private TestService testService;
+
+    //Create Test
+    @RequestMapping(value ="/create",method = RequestMethod.POST)
+    public Test create(@RequestBody Test test)
+    {
+        return testService.create(test);
+    }
+
+    //Read Test
+    @GetMapping("/read/{id}")
+    public Test read(@PathVariable String id)
+    {
+        return testService.read(id);
+    }
+
+    //Update Test
+    @PostMapping("/update")
+    public Test update(@RequestBody Test test)
+    {
+        return testService.update(test);
+    }
+
+    //Delete Test
+    @DeleteMapping("/delete/{id}")
+    public Boolean delete(@PathVariable String id)
+    {
+        return testService.delete(id);
+    }
+
+    //getAll
+    public Set<Test> getAll() {return testService.getAll();}
 }
