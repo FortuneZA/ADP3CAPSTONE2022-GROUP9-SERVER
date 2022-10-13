4b00c7a7c5899bbcefbf635f58814f5ebbedafd6
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-14 00:44:59 +0200

    Update TestController.java

--- src/main/java/za/ac/cput/controller/impl/TestController.java
+++ src/main/java/za/ac/cput/controller/impl/TestController.java
@@ -1,4 +1,56 @@
 package za.ac.cput.controller.impl;
 
+package za.ac.cput.controller.impl;
+
+/*
+ * Name:Themba
+ *Surname:Khanyile
+ *StudentNumber:217238173
+ *Date:13 October 2022
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.entity.Course;
+import za.ac.cput.service.impl.CourseService;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/course")
 public class TestController {
+
+    @Autowired
+    private CourseService courseService;
+
+    //Create course
+    @RequestMapping(value ="/create",method = RequestMethod.POST)
+    public Course create(@RequestBody Course course)
+    {
+        return courseService.create(course);
+    }
+
+    //Read course
+    @GetMapping("/read/{id}")
+    public Course read(@PathVariable String id)
+    {
+        return courseService.read(id);
+    }
+
+    //Update course
+    @PostMapping("/update")
+    public Course update(@RequestBody Course course)
+    {
+        return courseService.update(course);
+    }
+
+    //Delete course
+    @DeleteMapping("/delete/{id}")
+    public Boolean delete(@PathVariable String id)
+    {
+        return courseService.delete(id);
+    }
+
+    //getAll
+    public Set<Course> getAll() {return courseService.getAll();}
 }
