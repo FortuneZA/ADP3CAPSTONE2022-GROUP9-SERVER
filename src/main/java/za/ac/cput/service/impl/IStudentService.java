da73ca40c97f70d7b3076cf3358806637cccb046
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-18 15:59:25 +0200

    Update IStudentService.java

--- src/main/java/za/ac/cput/service/impl/IStudentService.java
+++ src/main/java/za/ac/cput/service/impl/IStudentService.java
@@ -9,7 +9,7 @@ import java.util.Set;
  *Date: 13 October 2022
  */
 
-public interface IStudentService {
+public interface IStudentService extends IStudentService<Student,String>{
     Student create(Student student);
 
     Object read(String studentId);
