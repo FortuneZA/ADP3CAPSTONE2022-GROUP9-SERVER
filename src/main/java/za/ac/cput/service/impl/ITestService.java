bbdd0f839374de5bd065ac7951e9dc614c393115
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-18 16:04:08 +0200

    Update ITestService.java

--- src/main/java/za/ac/cput/service/impl/ITestService.java
+++ src/main/java/za/ac/cput/service/impl/ITestService.java
@@ -3,10 +3,12 @@ package za.ac.cput.service.impl;
 import za.ac.cput.entity.Test;
 import za.ac.cput.service.IService;
 import java.util.Set;
-
-/*
-
+/*Name:Themba
+ *Surname:Khanyile
+ *StudentNumber:217238173
+ *Date: 13 October 2022
  */
+
 public interface ITestService extends IService<Test,String>{
    
     Set<Object> getAll();
