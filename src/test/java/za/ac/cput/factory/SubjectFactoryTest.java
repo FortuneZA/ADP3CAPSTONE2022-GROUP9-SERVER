05f927fd1ec40cdeb91eae16fc6c32f67c03af73
Project3Assignment <mfortuin509@gmail.com>
2022-10-20 21:58:05 +0200

    -Changed ID'S to type LONG

---
+++ src/test/java/za/ac/cput/factory/SubjectFactoryTest.java
@@ -0,0 +1,59 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Subject;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SubjectFactoryTest {
+
+
+    private final  Subject subject = SubjectFactory.createSubject("Information Management",36,"AF10");
+    private final  Subject subject2 = SubjectFactory.createSubject("Information Systems",36,"WR10");
+
+    private Subject subject3 = subject2;
+
+    //Successful Create
+    @Test
+     void successfulCreateSubject()
+    {
+        assertAll(
+                ()->assertNotNull(subject),
+                ()->assertEquals("FA12",subject.getLecturerID())
+
+        );
+    }
+
+    //Test Equality
+    @Test
+    void testEquality()
+    {
+        assertEquals(subject3,subject2);
+    }
+
+    //TIMEOUT
+    @Test
+    public void Timeout()
+    {
+        successfulCreateSubject();
+    }
+
+    //DISABLED
+    @Test
+    @Disabled
+    public void Disabled()
+    {
+       testEquality();
+    }
+
+    //Build Verification
+    @Test
+    public void TestNull()
+    {
+        Subject subject2 = SubjectFactory.createSubject("Information Management",36,"AF10");
+        assertNotEquals(subject2,null);
+    }
+
+}
