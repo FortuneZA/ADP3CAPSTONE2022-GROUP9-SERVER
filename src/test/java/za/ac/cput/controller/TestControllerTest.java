380644ca1b95845b17f60f690603769f911a78aa
Themba-217238173 <80292182+Themba-217238173@users.noreply.github.com>
2022-10-24 05:10:14 -0700

    Update TestControllerTest.java

--- src/test/java/za/ac/cput/controller/TestControllerTest.java
+++ src/test/java/za/ac/cput/controller/TestControllerTest.java
@@ -4,7 +4,7 @@ package za.ac.cput.controller;
  *StudentNumber:217238173
  *Date: 13 October 2022
  */
-/*
+
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -12,17 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import za.ac.cput.entity.Test;
-import za.ac.cput.factory.CourseFactory;
-import za.ac.cput.factory.TestFactory;
-import static org.junit.jupiter.api.Assertions.*;
+import za.ac.cput.entity.TestModel;
+import za.ac.cput.factory.TestModelFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
     @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TestControllerTest {
+class TestModelControllerTest {
 
-    private static Test test = new TestFactory().createTest("APPDEV01","ICT APPLICATION DEVELOPMENT","Fundamentals of Software Development","ICT01");
-    private Test testCourse;
+    private static TestModel test = new TestModelFactory().createTest("25AS","Advanced","25 October 2022","1Hour",25);
+    private TestModel testTest;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -33,27 +34,27 @@ class TestControllerTest {
     void a_create(){
         String url = BASE_URL + "/create";
         System.out.println("URL: "+url);
-        ResponseEntity<Test> postResponse = restTemplate.postForEntity(url,test,Test.class);
+        ResponseEntity<TestModel> postResponse = restTemplate.postForEntity(url,test,TestModel.class);
 
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
         assertEquals(postResponse.getStatusCode(), HttpStatus.OK);
         assertEquals(test.getTestId(),postResponse.getBody().getTestId());
-        testCourse = postResponse.getBody();
-        System.out.println("Course created: "+testCourse.toString());
+        testTest = postResponse.getBody();
+        System.out.println("Course created: "+testTest.toString());
     }
 
     @Test
     void b_read(){
         String url = BASE_URL +"/read/"+test.getTestId();
         System.out.println("URL: "+url);
-        ResponseEntity<Test> response = restTemplate.getForEntity(url,Test.class);
+        ResponseEntity<TestModel> response = restTemplate.getForEntity(url,TestModel.class);
 
         assertEquals(test.getTestId(),response.getBody().getTestId());
 
 
-        testCourse = response.getBody();
-        System.out.println("Reading: "+testCourse.toString());
+        testTest =  response.getBody();
+        System.out.println("Reading: "+testTest.toString());
     }
 
     @Test
@@ -61,18 +62,26 @@ class TestControllerTest {
 
         String url = BASE_URL + "/update/";
         System.out.println("URL: " + url);
-        Test updateTest = new Test.Builder().copy(test).setTestId("ARCH01").setCourseName("Architecture Development").setCourseDescription("Fundamentals of architecture studies").setDepartmentId("SJC2").build();
-        ResponseEntity<Test> postResponse = restTemplate.postForEntity(url,updateTest,Test.class);
+        TestModel updateTest = new TestModel
+                .Builder()
+                .copy(test)
+                .setSubjectId("INM77")
+                .setTestName("Information Systems Paper 2")
+                .setTestDate("12 November 2022")
+                .setDuration("2 Hours")
+                .setResultInPercent(30)
+                .build();
+        ResponseEntity<TestModel> postResponse = restTemplate.postForEntity(url,updateTest,TestModel.class);
 
         assertNotNull(postResponse.getBody());
 
-        testCourse = postResponse.getBody();
-        System.out.println("Test Updated: "+testCourse.toString());
+        testTest =  postResponse.getBody();
+        System.out.println("Test Updated: "+testTest.toString());
 
     }
 
     @Test
-    void d_delete() {
+    void d_getAll() {
 
         String url = BASE_URL + "/getAll";
         System.out.println("URL: " + url);
@@ -80,12 +89,18 @@ class TestControllerTest {
         HttpEntity<String> entity = new HttpEntity(null,headers);
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
 
-        System.out.println("Show all Test: ");
+        System.out.println("Show all Tests: ");
         System.out.println(response);
         System.out.println(response.getBody());
 
     }
+    @Test
+    void e_delete()
+    {
+        String url = BASE_URL + "/delete" + test.getTestId();
+        System.out.println("Test Deleted Successfuly!");
+        restTemplate.delete(url);
+    }
 
 
 }
-*/
